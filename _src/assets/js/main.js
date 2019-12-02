f668d81b178bad361233b08e4284204567a7e721
slizbeth <stefany.lizbeth@gmail.com>
2019-12-02 17:19:55 +0100

    add change color favourites

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,8 @@ console.log('>> Ready :)');
 const inputSerie = document.querySelector('#user-serie');
 const buttonSearch = document.querySelector('#search-button');
 const elementUl = document.querySelector('#shows-list');
+const elementUlFav = document.querySelector('shows-list--favs');
+let favShows = [];
 
 //****CONEXIÓN CON LA API****//
 const connectToAPI = () => {
@@ -17,25 +19,31 @@ const connectToAPI = () => {
 
 //****MOSTRAR DATOS****//
 const showData = (dataResponse) => {
+    elementUl.innerHTML = '';
     for (let i=0; i<dataResponse.length; i++) {
 
         const liElement = document.createElement('li');
-        const liElementText = document.createElement('li');
         const imgElement = document.createElement('img');
-        const spanElement = document.createElement('span');
-        const spanContent = document.createTextNode(dataResponse[i].show.name);
+        const pElement = document.createElement('p');
+        const pContent = document.createTextNode(dataResponse[i].show.name);
+        liElement.classList.add('color');
         let imagen = dataResponse[i].show.image;
         if(imagen === null){
-            imgElement.src = 'https://via.placeholder.com/210x295';
+            imgElement.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV.';
         }else{
             imgElement.src = dataResponse[i].show.image.medium;
         }
-        spanElement.appendChild(spanContent);
+        pElement.appendChild(pContent);
         liElement.appendChild(imgElement);
-        liElementText.appendChild(spanElement);
-        elementUl.appendChild(liElementText);
+        liElement.addEventListener('click', addFav);
+        liElement.appendChild(pElement);
         elementUl.appendChild(liElement);
     }
+}
+
+//****AÑADIR A FAVORITOS****//
+const addFav = (event) => {
+    event.currentTarget.style = 'background-color:blue; color:#fff';
 }
 
 buttonSearch.addEventListener('click', connectToAPI);
