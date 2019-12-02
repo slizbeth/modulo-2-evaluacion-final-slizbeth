f0384b56bf172155a4c72210b65a63abcb4959d2
slizbeth <stefany.lizbeth@gmail.com>
2019-12-02 15:51:50 +0100

    add StarterKit, HTML structure and JS function connectApi

---
+++ public/assets/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+const urlBase = 'http://api.tvmaze.com/search/shows?q=';
+const inputSerie = document.querySelector('#user-serie');
+const buttonSearch = document.querySelector('#search-button');
+
+//****CONEXIÓN CON LA API****//
+const connectToAPI = () => {
+    const inputSerieValue = inputSerie.value;
+    fetch(`http://api.tvmaze.com/search/shows?q=${inputSerieValue}`)
+    .then((response) => response.json())
+    .then(dataResponse =>  showData(dataResponse))
+    .catch(err => console.log('error', err));
+}
+
+//****MOSTRAR DATOS****//
+const showData = (dataResponse) => {
+    console.log(dataResponse);
+}
+
+
+buttonSearch.addEventListener('click', connectToAPI);
+//# sourceMappingURL=main.js.map
